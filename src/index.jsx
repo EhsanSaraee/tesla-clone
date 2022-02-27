90e7cbf8d79a424bc57f850217f98451647dbf22
ehsan <ehsansaraee6570@yahoo.com>
2022-02-27 19:35:23 +0330

    initial setup

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+   <Provider store={store}>
+      <App />
+   </Provider>,
+   rootElement
+);
