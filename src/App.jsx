7b18cc212cbbfeb6e4eeb2fe783dec231a1e5536
ehsan <ehsansaraee6570@yahoo.com>
2022-03-02 19:29:57 +0330

    adding login/logout check functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import HeaderMenu from './components/HeaderMenu';
 import Login from './components/Login';
@@ -8,11 +8,31 @@ import Menu from './components/Menu';
 import PrivateRoute from './components/PrivateRoute';
 import SignUp from './components/SignUp';
 import TeslaAccount from './components/TeslaAccount';
-import { selectUser } from './features/userSlice';
+import { login, logout, selectUser } from './features/userSlice';
+import { auth } from './firebase';
 
 const App = () => {
    const [isMenuOpen, setIsMenuOpen] = useState(false);
    const user = useSelector(selectUser);
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      auth.onAuthStateChanged((userAuth) => {
+         if (userAuth) {
+            // User is signed in
+            dispatch(
+               login({
+                  email: userAuth.email,
+                  uid: userAuth.uid,
+                  displayName: userAuth.displayName,
+               })
+            );
+         } else {
+            // User is signed out
+            dispatch(logout());
+         }
+      });
+   }, [dispatch]);
 
    return (
       <BrowserRouter>
